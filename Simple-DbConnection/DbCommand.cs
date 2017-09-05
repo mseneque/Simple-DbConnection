a76a350a2ddcd64fcf21ff1ee39286b098475b37
mseneque <mattseneque@gmail.com>
2017-09-05 11:40:11 +0800

    Adds Database Execute Command Functionality

---
+++ Simple-DbConnection/DbCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Simple_DbConnection
+{
+    public class DbCommand
+    {
+        private readonly DbConnection _dbConnection;
+        private readonly string _instruction;
+
+        // Constructor
+        public DbCommand(DbConnection dbConnection, string instruction)
+        {
+            _dbConnection = dbConnection;
+            _instruction = instruction;
+            if (dbConnection == null)
+                throw new Exception("You must pass an instance of DbConnection to create a DbCommand.");
+        }
+
+        // Methods
+        public void Execute()
+        {
+            _dbConnection.Open();
+            Console.WriteLine("Execute: {0}", _instruction);
+            _dbConnection.Close();
+        }
+    }
+
+}
